b10fcb717c919d8fc580eea3352fa8cb0f0aac49
Soniya Patel <soniya.patel@infostretch.com>
2019-08-23 14:32:14 +0530

    05 Hard coded ratings in catalog service

--- movie-catalog-service/src/main/java/demo/microservice/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/demo/microservice/moviecatalogservice/resources/CatalogResource.java
@@ -1,7 +1,9 @@
 package demo.microservice.moviecatalogservice.resources;
+import demo.microservice.moviecatalogservice.model.Rating;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,13 @@ public class CatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("Test", "Test Desc", 4));
+    	List<Rating> ratingsList = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
+        );
+
+        return ratingsList.stream()
+                .map(rating -> new CatalogItem("Name", "Desc", rating.getRating()))
+                .collect(Collectors.toList());
     }
 }
