9e44e7a185ff602c629ceb59e4fad691842e6995
Soniya Patel <soniya.patel@infostretch.com>
2019-08-23 15:23:05 +0530

    07 Calling Ratings Data API

--- movie-catalog-service/src/main/java/demo/microservice/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/demo/microservice/moviecatalogservice/resources/CatalogResource.java
@@ -1,5 +1,4 @@
 package demo.microservice.moviecatalogservice.resources;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import demo.microservice.moviecatalogservice.model.CatalogItem;
 import demo.microservice.moviecatalogservice.model.Movie;
-import demo.microservice.moviecatalogservice.model.Rating;
+import demo.microservice.moviecatalogservice.model.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -22,16 +21,19 @@ public class CatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-    	List<Rating> ratingsList = Arrays.asList(
-                new Rating("1234", 3),
-                new Rating("5678", 4)
-        );
+    	UserRating userRating = restTemplate.getForObject("http://localhost:8082/ratingsdata/user/" + userId, UserRating.class);
 
-        return ratingsList.stream()
+        return userRating.getRatings().stream()
         		.map(rating -> {
                     Movie movie = restTemplate.getForObject("http://localhost:8083/movies/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(), "Description", rating.getRating());
                 })
                 .collect(Collectors.toList());
+        
+        /*
+        Alternative WebClient way
+        Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
+        .retrieve().bodyToMono(Movie.class).block();
+        */ 
     }
 }
