20bf774a2bc1d688c04be8963be5e7944857f2cc
Soniya Patel <soniya.patel@infostretch.com>
2019-08-23 14:41:25 +0530

    06 Used RestTemplate to call Movie Info API

--- movie-catalog-service/src/main/java/demo/microservice/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/demo/microservice/moviecatalogservice/resources/CatalogResource.java
@@ -1,19 +1,24 @@
 package demo.microservice.moviecatalogservice.resources;
-import demo.microservice.moviecatalogservice.model.Rating;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import demo.microservice.moviecatalogservice.model.CatalogItem;
+import demo.microservice.moviecatalogservice.model.Movie;
+import demo.microservice.moviecatalogservice.model.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class CatalogResource {
+	
+	@Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -23,7 +28,10 @@ public class CatalogResource {
         );
 
         return ratingsList.stream()
-                .map(rating -> new CatalogItem("Name", "Desc", rating.getRating()))
+        		.map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8083/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                })
                 .collect(Collectors.toList());
     }
 }
